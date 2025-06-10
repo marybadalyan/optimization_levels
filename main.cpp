75c7534679189555fdc45d8d786a97abe984ad5a
marybadalyan <marybadalyangdh@gmail.com>
2025-06-10 18:48:31 +0400

    Add color-coded output formatting

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 #include <fstream>
 #include <string>
 
+
+
+namespace Color {
+    const char* RESET = "\033[0m";
+    const char* BOLD_CYAN = "\033[1;36m";
+    const char* BOLD_YELLOW = "\033[1;33m";
+    const char* BOLD_GREEN = "\033[1;32m";
+    const char* BOLD_MAGENTA = "\033[1;35m";
+}
+
 int run(){
     const size_t N = 100'000'000; // 100 million elements
     std::vector<double> data(N, 1.0); // initialize all elements to 1.0
@@ -31,9 +41,10 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     std::string assemblyFilePath = argv[1];
-    std::cout << "Assembly file path: " << assemblyFilePath << "\n";
-
-    std::cout << "Time: " << elapsed.count() << " seconds\n";
+    std::string optLevel = argv[2]; 
+    std::cout << Color::BOLD_CYAN << "Assembly Path:      " << assemblyFilePath << Color::RESET << std::endl;
+    std::cout << Color::BOLD_MAGENTA << "Optimization Level: " << optLevel << Color::RESET << std::endl;
+    std::cout << Color::BOLD_YELLOW << "Time: " << elapsed.count() << " seconds\n" << Color::RESET;
 
     std::ifstream asmFile(assemblyFilePath);
     if (!asmFile) {
@@ -64,7 +75,7 @@ int main(int argc, char* argv[]) {
     }
     asmFile.close();
 
-    std::cout << "Total instructions in run() function compiled: " << count << "\n";
+    std::cout << Color::BOLD_GREEN << "Total instructions in run() function compiled: " << count << "\n" << Color::RESET;
 
     return 0;
 }
